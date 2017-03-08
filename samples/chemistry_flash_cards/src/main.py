1d80df90c8760be4b6febc79aaf43159d85133ca
Timothy Cloninger <timothy.cloninger@lifechurch.tv>
2017-03-08 12:57:34 -0600

    Remove unnecessary code (looks like a port from the trivia template).

--- samples/chemistry_flash_cards/src/main.py
+++ samples/chemistry_flash_cards/src/main.py
@@ -14,9 +14,8 @@ import random
 
 
 # ------- Skill specific business logic -------
-ANSWER_COUNT = 1
 GAME_LENGTH = 5
-CARD_TITLE = "Chemistry Flash Cards"  # Be sure to change this for your skill.
+SKILL_NAME = "Chemistry Flash Cards"
 
 # When editing your questions pay attention to your punctuation.
 # Make sure you use question marks or periods.
@@ -157,7 +156,7 @@ def get_welcome_response():
     add those here
     """
 
-    speech_output = ("Let's test your skills with FlashCards. I will ask you " +
+    intro = ("Let's test your skills with FlashCards. I will ask you " +
                      str(GAME_LENGTH) + " questions, try to get as many right" +
                      "as you can. Just say the answer. Let's begin. ")
     should_end_session = False
@@ -166,29 +165,19 @@ def get_welcome_response():
 
     current_questions_index = 0
 
-    # Generate a random index for the correct answer, from 0 to 3
-    correct_answer_index = math.floor(random.random() * (ANSWER_COUNT))
-    round_answers = populate_round_answers(game_questions[current_questions_index], 0, correct_answer_index)
-
     spoken_question = QUESTIONS[game_questions[current_questions_index]].keys()[0]
-    reprompt_text = spoken_question
 
-    # Update code to deal with multiple choice answers
-    for i in range(0, ANSWER_COUNT):
-        reprompt_text = reprompt_text + ""
-
-    speech_output = speech_output + reprompt_text
-    attributes = {"speech_output": reprompt_text,
-                  "reprompt_text": reprompt_text,
+    speech_output = intro + spoken_question
+    attributes = {"speech_output": speech_output,
+                  "reprompt_text": spoken_question,
                   "current_questions_index": current_questions_index,
-                  "correct_answer_index": correct_answer_index + 1,
                   "questions": game_questions,
                   "score": 0,
                   "correct_answer_text": QUESTIONS[game_questions[current_questions_index]].values()[0][0]
                   }
 
     return build_response(attributes, build_speechlet_response(
-        CARD_TITLE, speech_output, reprompt_text, should_end_session))
+        SKILL_NAME, speech_output, spoken_question, should_end_session))
 
 
 def populate_game_questions():
@@ -216,38 +205,6 @@ def populate_game_questions():
     return game_questions
 
 
-def populate_round_answers(game_question_indexes, correct_answer_index, correct_answer_target_location):
-    """
-    Get the answers for a given question, and place the correct answer
-    at the spot marked by the correct_answer_target_location variable.
-    Note that you can have as many answers as you want but
-    only ANSWER_COUNT will be selected.
-    """
-
-    answers = []
-    answers_copy = QUESTIONS[game_question_indexes].values()[0]
-
-    index = len(answers_copy)
-
-    if index < ANSWER_COUNT:
-        raise ValueError("Not enough answers for question.")
-
-    # Shuffle the answers, excluding the first element.
-    # If only 1 element no need to shuffle
-    if index > 1:
-        temp = answers_copy[1:]
-        random.shuffle(temp)
-        answers.append(answers_copy[0])
-        answers.append(temp[0:ANSWER_COUNT-1])
-
-        # Swap the correct answer into the target location
-        answers[0], answers[correct_answer_target_location] = answers[correct_answer_target_location], answers[0]
-    else:
-        answers = answers_copy
-
-    return answers
-
-
 def handle_answer_request(intent, session):
     attributes = {}
     should_end_session = False
@@ -262,7 +219,7 @@ def handle_answer_request(intent, session):
         speech_output = "There is no game in progress. " \
                         "Do you want to start a new game?"
         reprompt_text = speech_output
-        return build_response(attributes, build_speechlet_response(CARD_TITLE,
+        return build_response(attributes, build_speechlet_response(SKILL_NAME,
                               speech_output, reprompt_text, should_end_session))
     elif not answer_slot_valid and user_gave_up == "DontKnowIntent":
         # If the user provided answer isn't a number > 0 and < ANSWER_COUNT,
@@ -270,10 +227,9 @@ def handle_answer_request(intent, session):
         # into providing correct values.
         reprompt = session['attributes']['speech_output']
         speech_output = "Your answer must be a known element " + reprompt
-        return build_response(session['attributes'], build_speechlet_response(CARD_TITLE, speech_output, reprompt_text, should_end_session))
+        return build_response(session['attributes'], build_speechlet_response(SKILL_NAME, speech_output, reprompt_text, should_end_session))
     else:
         game_questions = session['attributes']['questions']
-        correct_answer_index = session['attributes']['correct_answer_index']
         current_score = session['attributes']['score']
         current_questions_index = session['attributes']['current_questions_index']
         correct_answer_text = session['attributes']['correct_answer_text']
@@ -300,32 +256,26 @@ def handle_answer_request(intent, session):
             reprompt_text = None
             should_end_session = True
             return build_response(session['attributes'],
-                                  build_speechlet_response(CARD_TITLE, speech_output, reprompt_text, should_end_session))
+                                  build_speechlet_response(SKILL_NAME, speech_output, reprompt_text, should_end_session))
         else:
             current_questions_index += 1
             spoken_question = QUESTIONS[game_questions[current_questions_index]].keys()[0]
-            # Generate a random index for the correct answer, from 0 to 3
-            correct_answer_index = math.floor(random.random() * (ANSWER_COUNT))
-            round_answers = populate_round_answers(game_questions[current_questions_index], current_questions_index, correct_answer_index)
             reprompt_text = spoken_question
-            for i in range(0, ANSWER_COUNT):
-                reprompt_text = reprompt_text + ""
 
             speech_output = "" if user_gave_up == "DontKnowIntent" else "That answer is "
             speech_output = (speech_output + speech_output_analysis +
                              "Your score is " +
                              str(current_score) + '. ' + reprompt_text)
-            attributes = {"speech_output": reprompt_text,
+            attributes = {"speech_output": speech_output,
                           "reprompt_text": reprompt_text,
                           "current_questions_index": current_questions_index,
-                          "correct_answer_index": correct_answer_index + 1,
                           "questions": game_questions,
                           "score": current_score,
                           "correct_answer_text": QUESTIONS[game_questions[current_questions_index]].values()[0][0]
                           }
 
             return build_response(attributes,
-                                  build_speechlet_response(CARD_TITLE, speech_output, reprompt_text,
+                                  build_speechlet_response(SKILL_NAME, speech_output, reprompt_text,
                                                            should_end_session))
 
 
@@ -347,12 +297,11 @@ def handle_repeat_request(intent, session):
 
 def handle_get_help_request(intent, session):
     attributes = {}
-    card_title = "Flash Cards"
     speech_output = ("You can begin a game by saying start a new game, or, "
                      "you can say exit... What can I help you with?")
     reprompt_text = "What can I help you with?"
     should_end_session = False
-    return build_response(attributes, build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
+    return build_response(attributes, build_speechlet_response(SKILL_NAME, speech_output, reprompt_text, should_end_session))
 
 
 def handle_finish_session_request(intent, session):
